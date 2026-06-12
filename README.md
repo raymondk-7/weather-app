fd3ea2999d926c0b61eee66e38416e4d8959aa45
noventaysiete <raykabut@gmail.com>
2026-06-12 13:42:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# weather-app
+weather app made in react + vite that uses WeatherAPI
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Weather from './components/Weather'
+
+
+/* need to import google fonts in the .css file */
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Weather/>
+    </div>
+  )
+}
+
+export default App
+
